25a0f9b216aea9aacbce0780e4aa863c2eb3d575
lanomade <ganne.hannah@gmail.com>
2022-09-21 21:22:21 +0200

    add href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ export default function Header() {
             <nav>
                 <ul>
                     <li><img src={logo} alt="logo of SportSee" /></li>
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Profil</a></li>
-                    <li><a href="#">Réglage</a></li>
-                    <li><a href="#">Communauté</a></li>
+                    <li><a href="/accueil">Accueil</a></li>
+                    <li><a href="/profil">Profil</a></li>
+                    <li><a href="/reglage">Réglage</a></li>
+                    <li><a href="/communaute">Communauté</a></li>
                 </ul>
             </nav>
         </header>
