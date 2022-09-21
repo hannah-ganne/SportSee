4e34136b5b48606714f70474b5c322cbef20573b
lanomade <ganne.hannah@gmail.com>
2022-09-21 15:48:57 +0200

    render components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Main from './components/Main'
 
 export default function App() {
-
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Main />
+    </>
+  )
 }
 
